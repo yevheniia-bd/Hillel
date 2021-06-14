b2eee63b58e2e07014976fc9de912b7f5101260d
YEVHENIIA <YEVHENIIA.BENDERUK@GMAIL.COM>
2021-06-14 11:34:53 +0300

    First_commit

---
+++ venv/Lesson_1.py
@@ -0,0 +1,53 @@
+1.import this - The Zen of Python(The rules)
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+
+2. import antigravity - Histiry of Python https://xkcd.com/353/
+
+3. import __hello__ - Hello world!
+
+4.  from __future__ import braces -  File "<ipython-input-4-6d5c5b2f0daf>", line 1
+    from __future__ import braces
+    ^
+SyntaxError: not a chance
+
+5................:
+
+6.
+q=int(input())
+w=int(input())
+print(q,w)
+
+q=int(input())
+w=int(input())
+print(q+w)
+
+q=int(input())
+w=int(input())
+print(q//w)
+
+q=int(input())
+w=int(input())
+print(q%w)
+
+q=int(input())
+w=int(input())
+print(q**w)
